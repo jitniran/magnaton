a7662415c31c78670c2f7a8c6f0bacfe5b631e20
jitniran <jithsakes@gmail.com>
2018-10-16 17:37:48 +0530

    added base product design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Magnaton" });
+});
+
+// GET product page
+router.get("/product", function(req, res, next) {
+  res.render("product", { title: "Magnaton" });
 });
 
 module.exports = router;
